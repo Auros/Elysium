0d25db6a9e75c2953a6521d9ad8ea1ec36e1947d
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-09-14 08:42:56 -0400

    Created NotifiableBehaviour

--- Elysium/Assets/Elysium/Runtime/ViewModelDefinition.cs
+++ Elysium/Assets/Elysium/Runtime/ViewModelDefinition.cs
@@ -1,9 +1,41 @@
+using Elysium.Components;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Elysium
 {
-    public class ViewModelDefinition : MonoBehaviour
+    /// <summary>
+    /// Acts as a bridge to define a view model.
+    /// </summary>
+    [PublicAPI, DisallowMultipleComponent]
+    public class ViewModelDefinition : NotifiableBehaviour
     {
-        
+        [SerializeField]
+        private object? _viewModel;
+
+        /// <summary>
+        /// The View Model in this definition.
+        /// </summary>
+        public object? ViewModel
+        {
+            get => _viewModel;
+            set => SetField(ref _viewModel, value);
+        }
+
+        private void OnEnable()
+        {
+            // TODO: Subscribe to static binding UI system
+        }
+
+        private void OnDisable()
+        {
+            // TODO: Unsubscribe from static binding UI SYSTEM
+        }
+
+        private void OnTransformChildrenChanged()
+        {
+            print("child changed");
+            // TODO: Check registered bindings to ensure that we still own them.
+        }
     }
 }
